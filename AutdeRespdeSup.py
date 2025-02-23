9d6e1ad0560cfa619c57e705055ff7a71dbca81a
Joel Paes <jodenis@outlook.com>
2025-02-23 18:11:03 -0300

    parte 1 edição

---
+++ AutdeRespdeSup.py
@@ -0,0 +1,16 @@
+import http.client
+import json
+def obter_endereco_por_cep(cep):
+    conexao = http.client.HTTPConnection('viacep.com.br')
+    conexao.request('GET', f"/ws/{cep}/json/")
+    resposta = conexao.getresponse()
+    dados = resposta.read()
+    endereco = json.loads(dados.decode('utf-8'))
+    conexao.close()
+    if 'erro' in endereco:
+        return endereco
+    else:
+        return "CEP não encontrado."
+cep_exemplo = "01001000"
+endereco_resultado = obter_endereco_por_cep(cep_exemplo)
+print(endereco_resultado)
