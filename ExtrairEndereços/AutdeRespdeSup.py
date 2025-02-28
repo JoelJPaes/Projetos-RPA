67f0151f4b591f2577a908b696656bf21b72c208
Joel Paes <jodenis@outlook.com>
2025-02-28 08:13:23 -0300

    adicionando pandas e scripts de aulas

--- ExtrairEndereços/AutdeRespdeSup.py
+++ ExtrairEndereços/AutdeRespdeSup.py
@@ -1,5 +1,6 @@
 import http.client
 import json
+
 def obter_endereco_por_cep(cep):
     conexao = http.client.HTTPConnection('viacep.com.br')
     conexao.request('GET', f"/ws/{cep}/json/")
@@ -7,10 +8,11 @@ def obter_endereco_por_cep(cep):
     dados = resposta.read()
     endereco = json.loads(dados.decode('utf-8'))
     conexao.close()
-    if 'erro' in endereco:
+    if 'erro' not in endereco:
         return endereco
     else:
         return "CEP não encontrado."
 cep_exemplo = "01001000"
 endereco_resultado = obter_endereco_por_cep(cep_exemplo)
 print(endereco_resultado)
+
