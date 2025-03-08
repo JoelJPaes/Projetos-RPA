afe6baf164b6f218b85164eddfd8eb4c2ff434b8
Joel Paes <jodenis@outlook.com>
2025-03-08 18:12:51 -0300

    editando

--- Estruturas de controle.py/tuplas.py
+++ Estruturas de controle.py/tuplas.py
@@ -1,17 +1,18 @@
-# 1. Criando uma tupla
-numeros = (1, 2, 3, 4, 5)
+# # 1. Criando uma tupla
+# numeros = (1, 2, 3, 4, 5)
 
-# 2. Acessando elementos
-print(numeros[2])  # 3
+# # 2. Acessando elementos
+# print(numeros[2])  # 3
 
-# 3. Desempacotamento de tupla
-a, b, c, d, e = numeros
-print(a, b, c, d, e)  # 1 2 3 4 5
+# # 3. Desempacotamento de tupla
+# a, b, c, d, e = numeros
+# print(a, b, c, d, e)  # 1 2 3 4 5
 
-# 4. Verificando se um valor está na tupla
-print(3 in numeros)  # True
+# # 4. Verificando se um valor está na tupla
+# print(3 in numeros)  # True
 
-# 5. Concatenando tuplas
-outra_tupla = (6, 7, 8)
-nova_tupla = numeros + outra_tupla
-print(nova_tupla)  # (1, 2, 3, 4, 5, 6, 7, 8)
+# # 5. Concatenando tuplas
+# outra_tupla = (6, 7, 8)
+# nova_tupla = numeros + outra_tupla
+# print(nova_tupla)  # (1, 2, 3, 4, 5, 6, 7, 8)
+
